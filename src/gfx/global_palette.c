1091fc1162f0e7441c54c877ec0508c49d012484
Matthew Keller <matthewscottkeller@gmail.com>
2021-08-19 17:29:16 -0500

    removed graphics from main program

--- src/gfx/global_palette.c
+++
@@ -1,7 +0,0 @@
-unsigned char global_palette[8] =
-{
-    0xff, 0xff, /*   0: rgb(255, 255, 255) */
-    0x00, 0x00, /*   1: rgb(  0,   0,   0) */
-    0x9c, 0xf3, /*   2: rgb(230, 231, 230) */
-    0xfe, 0x7b, /*   3: rgb(247, 251, 247) */
-};
