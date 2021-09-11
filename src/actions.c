f89bc3adfcbe281133321bc1345d781d29e5ea00
Matthew Keller <matthewscottkeller@gmail.com>
2021-09-11 10:50:25 -0500

    updated continent ownership

--- src/actions.c
+++ src/actions.c
@@ -21,6 +21,7 @@ void gainTerritory(user_t* user, territory_t* territory, gfx_sprite_t* map, uint
     user->nTerritories++;
     user->userTerritories[user->nTerritories - 1] = territory->id;
     territory->owner = user;
+    territory->nTroops = 1;
     pal[map->data[territory->x + territory->y * MAP_WIDTH] * 2] = pal[6 + user->id * 2];
     pal[map->data[territory->x + territory->y * MAP_WIDTH] * 2 + 1] = pal[7 + user->id * 2];
 }
@@ -54,25 +55,29 @@ bool checkContinent(user_t* user, continent_t* continent){
     return true;
 }
 
-void gainContinent(user_t* user, continent_t* continent){
-    if(checkContinent(user, continent)){
-        user->continents[user->nContinents] = continent->id;
-        user->nContinents++;
-        continent->owner = user;
-    }
-}
-
-void loseContinent(user_t* user, continent_t* continent){
-    if(!checkContinent(user, continent)){
-        int i;
-        for(i = 0; i < user->nContinents; i++){
-            if(user->continents[i] == continent->id){
-                for(; i < user->nContinents; i++){
-                    user->continents[i] = user->continents[i + 1];
-                }
-                user->nContinents--;
-                return;
+void updateContinentOwnership(continent_t** continents, uint8_t nContinents, user_t** users, uint8_t nUsers){
+    uint8_t i;
+    for(i = 0; i < nContinents; i++){ //For all continents
+        uint8_t j;
+        setNullContinent(continents[i]);
+        for(j = 0; j < nUsers; j++){ //For all users
+            if(checkContinent(continents[i], users[j])){
+                setOwnedContinent(users[j], continents[i]);
+                break;
             }
         }
     }
+}
+
+void setOwnedContinent(user_t* user, continent_t* continent){
+    continent->owner = user;
+}
+
+void setNullContinent(continent_t* continent){
+    continent->owner = NULL;
+}
+
+int userGain(user_t* user, continent_t** continents){
+    uint8_t gain = 3 + (user->nTerritories - 9) / 3;
+    uint8_t i;
 }
