c6cbc449dc2991c8a9c03a6a1e014b326e3348d5
Matthew Keller <matthewscottkeller@gmail.com>
2021-09-11 10:50:43 -0500

    moved gfx to initializers

--- src/main.c
+++ src/main.c
@@ -22,19 +22,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "dice.h"
-#include "gfx/gfx.h"
+#include "types.h"
 
 int main(void)
 {
+    
+    srand(rtc_Time());
+    initGraphics();
     gfx_Begin();
-    gfx_SetPalette(global_palette, 4, 0);
-    gfx_SetTextBGColor(0);
-    gfx_SetTextFGColor(1);
-    gfx_SetTextTransparentColor(0);
-    while(!os_GetCSC()){
-        gfx_ScaledTransparentSprite_NoClip(RiskMap, 0, 5, 2, 2);
-        gfx_PrintStringXY("123", 238, 41);
-    }
+    runTestFile();
     gfx_End();
+    
+    return 0;
 }
