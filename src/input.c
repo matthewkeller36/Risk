7b19b6092991b666cee96dbcf7d3968677684390
Matthew Keller <matthewscottkeller@gmail.com>
2022-02-07 13:06:05 -0600

    removed loop for fps

--- src/input.c
+++ src/input.c
@@ -2,18 +2,18 @@
 
 int8_t selectUserTerritory(territory_t *territories, uint8_t *territoryList, uint8_t nTerritories, int8_t *choice){
     uint8_t index;
-    do{
-        index = territories[territoryList[*choice]].palIndex;
-        gfx_palette[index] = gfx_palette[(COLORS_BEFORE_PLAYERS + territories[territoryList[*choice]].owner->id + 6)];
-        kb_scan_edge();
-        if(kb_Rising[7]){
-            
-            gfx_palette[index] = gfx_palette[(COLORS_BEFORE_PLAYERS + territories[territoryList[*choice]].owner->id)];
-            if((kb_Rising[7] & kb_Right) || (kb_Rising[7] & kb_Up)) *choice = (*choice + 1) % nTerritories;
-            if((kb_Rising[7] & kb_Left) || (kb_Rising[7] & kb_Down)) *choice = (*choice + nTerritories - 1) % nTerritories;
-        }
-        if(kb_Rising[6] & kb_Clear) return -1;
-    }while(!(kb_Rising[1] & kb_2nd));
-    gfx_palette[index] = gfx_palette[(COLORS_BEFORE_PLAYERS + territories[territoryList[*choice]].owner->id)];
-    return 1;
+    index = territories[territoryList[*choice]].palIndex;
+    gfx_palette[index] = gfx_palette[(COLORS_BEFORE_PLAYERS + territories[territoryList[*choice]].owner->id + 6)];
+    kb_scan_edge();
+    if(kb_Rising[7]){
+        gfx_palette[index] = gfx_palette[(COLORS_BEFORE_PLAYERS + territories[territoryList[*choice]].owner->id)];
+        if((kb_Rising[7] & kb_Right) || (kb_Rising[7] & kb_Up)) *choice = (*choice + 1) % nTerritories;
+        if((kb_Rising[7] & kb_Left) || (kb_Rising[7] & kb_Down)) *choice = (*choice + nTerritories - 1) % nTerritories;
+    }
+    if(kb_Rising[1] & kb_2nd){
+        gfx_palette[index] = gfx_palette[(COLORS_BEFORE_PLAYERS + territories[territoryList[*choice]].owner->id)];
+        return 1;
+    }
+    
+    return 0;
 }
