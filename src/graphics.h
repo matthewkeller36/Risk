05f0b933ed07e3e5be8ddadfa7f909e470587439
Matthew Keller <matthewscottkeller@gmail.com>
2022-03-05 13:48:06 -0600

    added displayText, exitCOnfirm

--- src/graphics.h
+++ src/graphics.h
@@ -4,13 +4,13 @@
 #include "types.h"
 
 void initGraphics();
-void printAll(game_t* game);
+void printAll(game_t* game, char *dispText);
 void printTerritoryName(territory_t* territory);
 void printTopBanner(game_t* game);
 void printLeftBanner(game_t* game);
-void printBottomBanner(game_t* game);
+void printBottomBanner(game_t* game, char *dispText);
 void printRightBanner(game_t* game);
 void itoa_custom(char* buffer, uint8_t num);
 void printCentered(char* str, int y);
-
+uint8_t exitConfirm();
 #endif
